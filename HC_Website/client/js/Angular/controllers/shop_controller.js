4b38b5d4d40d22796e7026ce6a2ef972d1739aef
Alec Barlow <abarlow85@gmail.com>
2016-09-28 11:31:39 -0700

    angular store data

--- HC_Website/client/js/Angular/controllers/shop_controller.js
+++ HC_Website/client/js/Angular/controllers/shop_controller.js
@@ -1,9 +1,15 @@
-happy_cup.controller('shop_controller', function ($scope){
+happy_cup.controller('shop_controller', function ($scope, content_factory){
+
+	content_factory.getPageContent('home', function(content){
+		$scope.products = content.products;
+	});
 
 	$scope.productDisplay = {
 		"showCoffee" : true,
 		"showMerch" : false
 	};
+
+
 	
 	$scope.showCoffee = function (){
 		$scope.productDisplay.showCoffee = true;
