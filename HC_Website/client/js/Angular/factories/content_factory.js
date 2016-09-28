0e22adee5ec6e9907b7799b7f0db9c43bd34bc34
Nigel <nkoh.hs@gmail.com>
2016-09-28 15:56:01 -0700

    added information to content controller

--- HC_Website/client/js/Angular/factories/content_factory.js
+++ HC_Website/client/js/Angular/factories/content_factory.js
@@ -15,10 +15,20 @@ happy_cup.factory('content_factory',function(){
 			};
 			content.about = {
 
-			};
-			content.blog = {
-
-			};
+			};			
+			content.blog = [
+				{
+					'title': 'Sustainable Business Oregon',
+					'post': '<a href="http://www.sustainablebusinessoregon.com/articles/2011/12/full-life-launches-sustainable-coffee.html?ana=twt" target="_blank">“Full Life Launches Sustainable Coffee Roasting Business”</a>',
+					'created_at': '2011-12-02T14:06:42+00:00',
+					'updated_at': '2011-12-02T14:06:42+00:00',
+				},
+				{
+					'title': 'The Little Red Hen',
+					'post': '<a href="http://www.alittleredhen.com/a_little_red_hen/2012/02/joe-vithayathil-enthusiastic-about-happy-cup-coffee.html" target="_blank">The Little Red Hen</a>'
+					'created_at': ''
+				}
+			];
 			content.contact = {
 
 			};
