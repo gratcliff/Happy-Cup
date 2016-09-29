e1f075b6e02b2d915b9ce6efce1ef1e8f951281d
Nigel <nkoh.hs@gmail.com>
2016-09-29 10:12:31 -0700

    added first blog post details

--- HC_Website/client/js/Angular/factories/content_factory.js
+++ HC_Website/client/js/Angular/factories/content_factory.js
@@ -18,17 +18,14 @@ happy_cup.factory('content_factory',function(){
 			};			
 			content.blog = [
 				{
-					'title': 'Sustainable Business Oregon',
+					'title': 'onlyinyourstate.com named Happy Cup one of the 12 unique shops to visit in Oregon',
 					'link_title': 'Full Life Launches Sustainable Coffee Roasting Business',
 					'url': 'http://www.sustainablebusinessoregon.com/articles/2011/12/full-life-launches-sustainable-coffee.html',
-					'text': '',
-					'created_at': '2011-12-02T14:06:42+00:00',
-					'updated_at': '2011-12-02T14:06:42+00:00',
+					'img_url': '../resources/images/blog/2016/01/21/1.jpg',
+					'text': '<p> Based in Portland, Happy Cup is a unique cafe and coffee roaster using ethicially traded coffee sourced from all over the world. The humanitarian, community oriented company offers employment to adults with disabilities and donates a percentage of their profits to vocational and recreational programming for the disabled community. Plus, their coffee is delicious! My personal favorite is the Boom Boom dark roast. Read the full article <a href="http://www.onlyinyourstate.com/oregon/coffee-shops-oregon/">here.</a></p>',
+					'created_at': '2016-01-21T16:15:37+00:00',
+					'updated_at': '2016-01-21T16:15:37+00:00',
 				},
-				{
-					'title': 'The Little Red Hen',
-					'post': '<a href="http://www.alittleredhen.com/a_little_red_hen/2012/02/joe-vithayathil-enthusiastic-about-happy-cup-coffee.html" target="_blank">The Little Red Hen</a>',
-				}
 			];
 			content.contact = {
 
