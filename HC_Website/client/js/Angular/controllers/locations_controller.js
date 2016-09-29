88c4cabb75b4dabbf722e80790e30615a5bf58f7
Gabe Ratcliff <gaberatcliff@gmail.com>
2016-09-28 17:58:59 -0700

    routine

--- HC_Website/client/js/Angular/controllers/locations_controller.js
+++ HC_Website/client/js/Angular/controllers/locations_controller.js
@@ -332,7 +332,7 @@ happy_cup.controller('locations_controller', function($scope, $http, $location){
 
 	$scope.locations = locations;
 	$scope.updateMap = function(address){
-		$http.get('https://maps.googleapis.com/maps/api/geocode/json?&address='+address+'&key=AIzaSyAgyegYtAmC9LHm4LQhrU8Drl7ub2TVQ3s').success(function(response){
+		$http.get('https://maps.googleapis.com/maps/api/geocode/json?&address='+address+'&key=*******').success(function(response){
 			if ($("#map-canvas").length>0) {
 				console.log(response.results[0]);
 				var map, myLatlng, myZoom, marker;
